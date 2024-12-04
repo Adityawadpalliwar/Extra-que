f43f5ccb8f27fe94c0792307a97acc6ab50169d7
Aditya wadpalliwar <adityawadpalliwar520@gmail.com>
2024-12-04 13:16:01 +0530

    both done

--- qu1.c
+++ qu1.c
@@ -6,13 +6,14 @@
 int main()
 {   int i,j;
 
-    int a=1,b=1,sum,greatest=0;
-    int l ,k;
+    //int a=1,b=1;
+    int sum,greatest=0,x,y;
+    int l=0,k=0;
     printf("ENTER THE NUMBER OF ROWS AND COLUMNS");
 
     int row,coloums;
     scanf("%d",&row);
-     scanf("%d",&coloums);
+    scanf("%d",&coloums);
 
   if(row>=2&&coloums<=100)
   {
@@ -26,162 +27,57 @@ int main()
             scanf("%d",&m[i][j]);
              }
          }
-  
 
-      
- 
-
-         for(i=0;i<row;i++)
-            {  sum=0;
-            if (i==0)
-              { for(j=0;j<coloums;j++)
-                {if (j==0)
-                { continue;
-                }
-                 if (j==coloums-1)
-                {   for(l=0;l<2;l++)
-                   {
-                     for(k=-1;k<1;k++)
-                     {   if(l==0&&k==0)
-                     continue;
-                      sum=sum+ m[i+l][j+k];
-                    }
-                    }
-                }
-
-               for(l=0;l<2;l++)
-                {
-                 for(k=-1;k<2;k++)
-                {   if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-                }
-                
-             if (sum>greatest)
-             {
-                a=i;
-                b=j;
-            }
-            if (sum==greatest)
-            {
-             if (sqrt(i*i+j*j)<sqrt(a*a+b*b))
-                a=i;
-                b=j;
-             }
-            }
-            }
-
-        if (i==row-1)
-        {      
-            for(j=0;j<coloums;j++)
-              {  if (j==0)
-               { for(l=-1;l<1;l++)
-                {
-                 for(k=0;k<2;k++)
-                   if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-                }
-               }
-            if (j==coloums-1)
-            {   for(l=0;l<2;l++)
-                {
-                 for(k=-1;k<1;k++)
-                {   if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-                }
-                }
-            else {for(l=0;l<2;l++)
-            {
-                for(k=-1;k<2;k++)
-                {  if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-            }}
-               
-             if (sum>greatest)
-             {
-                a=i;
-                b=j;
-            }
-            if (sum==greatest)
-            {
-             if (sqrt(i*i+j*j)<sqrt(a*a+b*b))
-                a=i;
-                b=j;
-             }
-
-        }
-
-            
-
-   
-        else
+        for(i=0;i<row;i++)
         {
-           for(j=0;j<coloums;j++)
-              {  if (j==0)
-               { for(l=-1;l<1;l++)
-                {
-                 for(k=0;k<2;k++)
-                   if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-                }
-               
-            if (j==coloums-1)
-            {   for(l=0;l<2;l++)
-                {
-                 for(k=-1;k<1;k++)
-                {   if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-                }
-            }
-            
-             else{ for(l=0;l<2;l++)
-            
-                for(k=-1;k<2;k++)
-                {  if(l==0&&k==0)
-                 continue;
-                  sum=sum+ m[i+l][j+k];
-                }
-            }
-              if (sum>greatest)
-             {
-                a=i;
-                b=j;
-            }
-            if (sum==greatest)
+         for(j=0;j<coloums;j++)
             {
-             if (sqrt(i*i+j*j)<sqrt(a*a+b*b))
-                a=i;
-                b=j;
-             } 
+              if(m[i][j]==0||(i==0&&j==0))
+              continue;
+              else
+              {  sum=0;
+                for(x=i-1;x<=i+1;x++)
+                {
+                  for(y=j-1;y<=j+1;y++)
+                  { 
+                    if(((x>=0&&x<row)&&(y>=0&&y<coloums))&&(x!=i||y!=j))
+                    {
+                      sum+=m[x][y];
+                    }
+                    else
+                    continue;
+                  }
+                }
+                if (sum>greatest)
+                {
+                  greatest =sum ;
+                  l=i;
+                  k=j;
+                }
+                if(sum==greatest)
+                {
+                  if((i+j)<(l+k))
+                  {
+                    l=i;
+                    k=j;
+                  }
+                   
+                }
+              }
             }
         }
-
-
-        
+  
+  
     
-    }
   }
-  
-    
-  
   else
   {
     printf("no sutaible girl found");
   }
 
 
-  printf("%d %d",a,b);
+  printf("%d:%d:%d",l+1,k+1,greatest);
+  return 0;
 
 }
 
